d6c4f15c69f1fd17e52a7ef025a84c0c437d0263
reeeham <reeehamgamal@gmail.com>
2021-12-26 22:59:52 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var viewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https://www.flickr.com/photos/onesharp/9648464288');
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    }
+}
+ko.applyBindings(new viewModel())
+
+
+
+
+
+
