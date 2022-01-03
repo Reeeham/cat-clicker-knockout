cddffd6873bdfd8f2f89546bbea2f79934473982
reeeham <reeehamgamal@gmail.com>
2022-01-03 23:17:22 +0200

    add more cats and seperate the model and viewmodel

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name : 'Tabby',
+        imgSrc : './img/434164568_fea0ad4013_z.jpeg',
+        imgAttribution :'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames :['Tabby','Tiger', 'Tiger', 'Shadow' , 'Sleepy']
+    },
+    {
+        clickCount: 0,
+        name : 'Tiger',
+        imgSrc : './img/22252709_010df3379e_z.jpeg',
+        imgAttribution :'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames :['Tiger']
+    },
+    {
+        clickCount: 0,
+        name : 'Scaredy',
+        imgSrc : './img/1413379559_412a540d29_z.jpeg',
+        imgAttribution :'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames :['Casper']
+    },
+    {
+        clickCount: 0,
+        name : 'Shadow',
+        imgSrc : './img/4154543904_6e2428c421_z.jpeg',
+        imgAttribution :'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames :['Shoopy']
+    },
+    {
+        clickCount: 0,
+        name : 'Rere',
+        imgSrc : './img/9648464288_2516b35537_z.jpeg',
+        imgAttribution :'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames :['Zzzzz']
+    },
+]
 
-var Cat = function() {
+var Cat = function(data) { //model
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames)
     this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('./img/434164568_fea0ad4013_z.jpeg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/onesharp/9648464288');
-    this.nicknames = ko.observableArray(['Tabby','Tiger', 'Tiger', 'Shadow' , 'Sleepy'])
-    this.clickCount = ko.observable(0);
+
     this.title = ko.computed(function(){
         var title;
         var clicks = this.clickCount();
@@ -25,12 +63,20 @@ var Cat = function() {
         return title;
     }, this);
 }
-var viewModel = function() {
-    this.currentCat = ko.observable( new Cat() );
+var viewModel = function() { 
+    var self = this; //refer to this view model
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem))
+    })
+    this.currentCat = ko.observable(this.catList()[0]);
     this.incrementCounter = function() {
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+        //this.currentCat
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
-   
+   this.setCurrentCat = function(clickedCat) { 
+       self.currentCat(clickedCat);
+   };
    
 }
 ko.applyBindings(new viewModel())
